edadbf0d555b82be1787686b59526ed30e7b910c
Leonid Boginskiy <ileonidbogin@gmail.com>
2020-03-21 19:09:35 +0300

    move Reg.cs

---
+++ AngelControl/AngelControl/Data/Class/Reg.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AngelControl.Data.Class {
+    public class Reg {
+        public int Id { get; set; }
+        public string Numcard { get; set; }
+        public string Lname { get; set; }
+        public string Fname { get; set; }
+        public string Pname { get; set; }
+
+
+    }
+}
