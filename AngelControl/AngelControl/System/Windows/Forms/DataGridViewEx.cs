23d40b064be11325aff3d918366c3fefb37dc41f
Leonid Boginskiy <ileonidbogin@gmail.com>
2020-03-25 13:15:12 +0300

    init

--- AngelControl/AngelControl/System/Windows/Forms/DataGridViewEx.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace System.Windows.Forms {
-    public class DataGridViewEx : DataGridView {
-        public DataGridViewEx() : base() {
-            SetStyle(ControlStyles.OptimizedDoubleBuffer | ControlStyles.AllPaintingInWmPaint, true);
-        }
-    }
-}
